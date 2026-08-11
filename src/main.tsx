b0a7df3c428e51e7711b9bc29089937b22ec6b42
Guo Yunhe <i@guoyunhe.me>
2026-08-11 12:42:09 +0800

    ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import xior from 'xior'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import xior from "xior";
 
-xior.defaults.baseURL = import.meta.env.VITE_API_URL
+xior.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,9 +5,9 @@ interface ViteTypeOptions {
 }
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL: string
+  readonly VITE_API_URL: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
